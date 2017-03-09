b46d6fa4bcd989ab04b7f2822d23421052b33efe
ryanss <ryanssdev@icloud.com>
2017-03-09 16:57:05 -0500

    Quiet curl and wget in Plex update script

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
-deburl=`curl https://plex.tv/api/downloads/1.json | egrep -m 1 -o 'http[^\"]+amd64.deb' | head -1`
+# Download newest Plex Media Server and install
+deburl=`curl -s https://plex.tv/api/downloads/1.json | egrep -m 1 -o 'http[^\"]+amd64.deb' | head -1`
 debfile=`echo $deburl | grep -o plexmediaserver_.*`
 
 if [ ! -f $debfile ]; then
-    wget $deburl
+    wget -q $deburl
     dpkg -i $debfile
     ls | grep plexmediaserver | grep -v $debfile | xargs rm
     kill -9 $(ps aux | grep -m1 Plex | awk '{print $2}') || true
