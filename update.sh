c8421242cb4743dfa6fd6aa57964adcc0254c966
ryanss <ryanssdev@icloud.com>
2017-03-09 16:57:32 -0500

    Set Plex Custom server access URL using CADDY_HOSTNAME
    Plex apps will not be able to connect to this Plex Media Server without
    this custom URL field set

--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
+# Set Plex Custom server access URL
+if ! grep -q customConnections /config/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml && [ $CADDY_HOSTNAME != "localhost" ];
+then
+    sed -i "s/\/>/\ customConnections=\"https:\/\/$CADDY_HOSTNAME:32400\"\/>/g" /config/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml;
+fi
+
+
 # Download newest Plex Media Server and install
 deburl=`curl -s https://plex.tv/api/downloads/1.json | egrep -m 1 -o 'http[^\"]+amd64.deb' | head -1`
 debfile=`echo $deburl | grep -o plexmediaserver_.*`
