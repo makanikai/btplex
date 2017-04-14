811b83d10fcf8698446eff1aebf948effe51963c
ryanss <ryanssdev@icloud.com>
2017-04-14 08:48:50 -0400

    Remove custom Transmission Dockerfile
    Disconnection issues; constant restarts

--- cronjob-transmission.sh
+++
@@ -1,4 +0,0 @@
-if ! ping -c 1 us-midwest.privateinternetaccess.com &> /dev/null;
-then
-    kill -9 $(ps aux | grep -m1 transmission | awk '{print $2}') || true;
-fi
--- cronjob.sh
+++ cronjob.sh
@@ -3,3 +3,4 @@ rclone --config /rclone.conf moveto /data/tvshows/ ACDcrypt:tvshows >> /config/r
 find /data/movies/* -type d -empty -delete
 find /data/tvshows/* -type d -empty -delete
 if [ -z "$(mount | grep /acd)" ]; then rclone --config /rclone.conf mount ACDcrypt: /acd; fi >> /config/rclone.log 2>&1
+/usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan
